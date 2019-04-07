03723457d62e805a657b95bc21a8cd8627bef224
deadcheat <deadcheat@users.noreply.github.com>
2019-04-07 10:12:09 +0900

    fixed indent

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -73,97 +73,98 @@ class _ManemoReceiptDialogState extends State<ManemoReceiptDialog> {
   Widget build(BuildContext context) {
     return new SimpleDialog(children: <Widget>[
       Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.centerLeft,
-                child: new Text(
-                  'Date',
-                  style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              child: new Text(
+                'Date',
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
                 ),
               ),
-              Container(
-                child: DateTimePickerFormField(
-                  inputType: InputType.date,
-                  format: DateFormat('yyyy-MM-dd'),
-                  editable: true,
-                  initialDate: new DateTime.now(),
-                  initialValue: new DateTime.now(),
-                  style: TextStyle(fontSize: 24.0),
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                      labelText: 'Date/Time', hasFloatingPlaceholder: false),
-                  onChanged: (dt) => setState(() => paidDate = dt),
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: new Text(
-                  'Total',
-                  style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
-                ),
-              ),
-              TextFormField(
-                controller: myController,
+            ),
+            Container(
+              child: DateTimePickerFormField(
+                inputType: InputType.date,
+                format: DateFormat('yyyy-MM-dd'),
+                editable: true,
+                initialDate: new DateTime.now(),
+                initialValue: new DateTime.now(),
+                style: TextStyle(fontSize: 24.0),
+                textAlign: TextAlign.center,
                 decoration: InputDecoration(
-                  prefix: Text("￥"),
+                    labelText: 'Date/Time', hasFloatingPlaceholder: false),
+                onChanged: (dt) => setState(() => paidDate = dt),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: new Text(
+                'Total',
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
                 ),
-                inputFormatters: [
-                  WhitelistingTextInputFormatter.digitsOnly,
-                ],
-                validator: _numberValidator,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                textAlign: TextAlign.right,
-                style: TextStyle(fontSize: 40.0),
               ),
-              new Padding(
-                padding: new EdgeInsets.all(8.0),
+            ),
+            TextFormField(
+              controller: myController,
+              decoration: InputDecoration(
+                prefix: Text("￥"),
               ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: new Text(
-                  'Which did you pay by?',
+              inputFormatters: [
+                WhitelistingTextInputFormatter.digitsOnly,
+              ],
+              validator: _numberValidator,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              textAlign: TextAlign.right,
+              style: TextStyle(fontSize: 40.0),
+            ),
+            new Padding(
+              padding: new EdgeInsets.all(8.0),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: new Text(
+                'Which did you pay by?',
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Radio<PaymentType>(
+                  value: PaymentType.cash,
+                  groupValue: _paymentType,
+                  onChanged: _setPaymentType,
+                ),
+                new Text(
+                  'Cash',
+                  style: new TextStyle(fontSize: 20.0),
+                ),
+                new Radio<PaymentType>(
+                  value: PaymentType.charge,
+                  groupValue: _paymentType,
+                  onChanged: _setPaymentType,
+                ),
+                new Text(
+                  'Charge',
                   style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
+                    fontSize: 20.0,
                   ),
                 ),
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Radio<PaymentType>(
-                    value: PaymentType.cash,
-                    groupValue: _paymentType,
-                    onChanged: _setPaymentType,
-                  ),
-                  new Text(
-                    'Cash',
-                    style: new TextStyle(fontSize: 20.0),
-                  ),
-                  new Radio<PaymentType>(
-                    value: PaymentType.charge,
-                    groupValue: _paymentType,
-                    onChanged: _setPaymentType,
-                  ),
-                  new Text(
-                    'Charge',
-                    style: new TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
-              ),
-              _addButton()
-            ],
-          ))
+              ],
+            ),
+            _addButton()
+          ],
+        ),
+      ),
     ]);
   }
 
