bc1e6bc9274dcbe267c33aef166207a536f9685c
deadcheat <deadcheat@users.noreply.github.com>
2019-04-16 13:52:18 +0900

    rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:manemo/dialog.dart';
+import 'package:manemo/receipttab.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: Monemo(title: 'Monemo'),
@@ -28,15 +19,6 @@ class MyApp extends StatelessWidget {
 
 class Monemo extends StatefulWidget {
   Monemo({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -132,12 +114,11 @@ class _MonemoState extends State<Monemo> {
       floatingActionButton: new FloatingActionButton(
           backgroundColor: const Color(0xFF0099ed),
           child: new Icon(Icons.add_circle),
-          onPressed: openDialog),
+          onPressed: openTab),
     );
   }
 
-  void openDialog() {
-    // showDialog(context: context, builder: (_) => ManemoReceiptDialog());
+  void openTab() {
     Navigator.push(
       context,
       MaterialPageRoute(
