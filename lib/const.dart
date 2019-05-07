9c2eaf743f387a358c15d2cbed91f231e57d673d
deadcheat <deadcheat@users.noreply.github.com>
2019-05-07 23:14:30 +0900

    added consts

--- lib/const.dart
+++ lib/const.dart
@@ -1,1 +1,14 @@
+import 'package:intl/intl.dart';
+
 const APPNAME = 'MANEMO';
+const LOCALE_JA_JP = 'ja_JP';
+const CURRENCY_NUMBER_FORMAT = '#,###';
+const DISPLAY_CURRENCY_NUMBER_FORMAT = '￥ #,###';
+const DATE_FORMAT_YYYY_MM = 'yyyy/MM';
+const DATE_FORMAT_YYYY_MM_DD = 'yyyy-MM-dd';
+const DISPLAY_WORD_CHARGE = 'Charge';
+const DISPLAY_WORD_CASH = 'Cash';
+
+class ConstantInstances {
+  static final dateFormat = DateFormat(DATE_FORMAT_YYYY_MM_DD);
+}
