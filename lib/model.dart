40826e97e4de379a4dd850a2272eb8e562158dd8
deadcheat <deadcheat@users.noreply.github.com>
2019-07-27 11:52:05 +0900

    - removed continuationType - added regularReceipt accessor

--- lib/model.dart
+++ lib/model.dart
@@ -15,7 +15,6 @@ class Receipt {
   int utime;
   String description;
   int price;
-  int continuationType;
   int paymentType;
 
   Receipt({
@@ -23,7 +22,6 @@ class Receipt {
     this.utime,
     this.description,
     this.price,
-    this.continuationType,
     this.paymentType,
   });
 
@@ -32,7 +30,6 @@ class Receipt {
         utime: m["utime"],
         description: m["description"],
         price: m["price"],
-        continuationType: m["continuation_type"],
         paymentType: m["payment_type"],
       );
 
@@ -41,7 +38,61 @@ class Receipt {
         "utime": utime,
         "description": description,
         "price": price,
-        "continuation_type": continuationType,
+        "payment_type": paymentType,
+      };
+
+  @override
+  String toString() {
+    return super.toString();
+  }
+}
+
+RegularReceipt regularReceiptFromJson(String str) {
+  final jsonData = json.decode(str);
+  return RegularReceipt.fromMap(jsonData);
+}
+
+String regularReceiptToJson(RegularReceipt data) {
+  final dyn = data.toMap();
+  return json.encode(dyn);
+}
+
+class RegularReceipt {
+  int id;
+  int utimeMonthFrom;
+  int utimeMonthTo;
+  int dayOfMonth;
+  String description;
+  int price;
+  int paymentType;
+
+  RegularReceipt({
+    this.id,
+    this.utimeMonthFrom,
+    this.utimeMonthTo,
+    this.dayOfMonth,
+    this.description,
+    this.price,
+    this.paymentType,
+  });
+
+  factory RegularReceipt.fromMap(Map<String, dynamic> m) => new RegularReceipt(
+        id: m["id"],
+        utimeMonthFrom: m["utime_month_from"],
+        utimeMonthTo: m["utime_month_to"],
+        dayOfMonth: m["day_of_month"],
+        description: m["description"],
+        price: m["price"],
+        paymentType: m["payment_type"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "utime_month_from": utimeMonthFrom,
+        "utime_month_to": utimeMonthTo,
+        "day_of_month": dayOfMonth,
+        "description": description,
+        "price": price,
         "payment_type": paymentType,
       };
 
